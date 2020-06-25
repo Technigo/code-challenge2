058c30a5ebe8d139b3ff11686678244a44166c0c
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2020-06-25 15:21:36 +0200

    Added styling and js-file for accordion

---
+++ script.js
@@ -0,0 +1,15 @@
+var acc = document.getElementsByClassName("accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("is-open");
+    
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
